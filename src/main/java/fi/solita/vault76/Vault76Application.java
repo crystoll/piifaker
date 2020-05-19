bb08c70f391a84cd0aaee5f3efc298eef7aa8aab
Arto Santala <arto.santala@solita.fi>
2020-05-19 11:57:35 +0300

    Initial import, initialized Spring Boot + Java 14 project

---
+++ src/main/java/fi/solita/vault76/Vault76Application.java
@@ -0,0 +1,13 @@
+package fi.solita.vault76;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vault76Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vault76Application.class, args);
+	}
+
+}
---
+++ src/test/java/fi/solita/vault76/Vault76ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.solita.vault76;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vault76ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
